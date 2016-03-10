f73fe2c0472178214290a8c08e200616e66c3302
John Flynn <johnflynn@users.noreply.github.com>
2016-03-10 17:49:09 +0000

    Tidy FilmStrip.

--- Source/FilmStrip.h
+++ Source/FilmStrip.h
@@ -16,49 +16,56 @@
 /**
     Slider that uses a PNG filmstrip for display
     --------------------------------------------
+    
+    Assumes:
+    - Vertical filmstrip.
+    - Square image frame to calculate number of frames automatically by image
+      size.
 
 */
 class FilmStrip  : public Slider
 {
 public:
-    FilmStrip (Image image, const int numFrames)
+    FilmStrip (Image image)
         : Slider {},
-          numFrames_ {numFrames},
-          filmStrip_ {image}
+          filmStrip_ {image},
+          height_    {filmStrip_.getHeight()},
+          width_     {filmStrip_.getWidth()},
+          numFrames_ {height_ / width_}
     {
-        //setTextBoxStyle(NoTextBox, 0, 0, 0);
+        jassert (height_ > width_);
+        jassert (numFrames_ > 0);
+
+        //setTextBoxStyle (NoTextBox, 0, 0, 0);
         setTextBoxStyle (TextBoxBelow, false, 80, 15);
         setColour (Slider::textBoxTextColourId, Colour (0xff404040));
         setColour (Slider::textBoxBackgroundColourId, Colour (0x10000000));
         setColour (Slider::textBoxOutlineColourId, Colour (0x00000000));
         setSliderStyle (RotaryHorizontalVerticalDrag);
-
-        frameHeight_ = filmStrip_.getHeight() / numFrames_;
-        frameWidth_ = filmStrip_.getWidth();
     }
     
     void paint(Graphics& g)
     {
-        const float sliderPos = (float) valueToProportionOfLength (getValue());
+        const float sliderPosition = static_cast<float> (valueToProportionOfLength (getValue()));
 
-        int value = sliderPos * (numFrames_ - 1);
+        int imagePositionRatio = sliderPosition * (numFrames_ - 1);
 
-        g.drawImage (filmStrip_, 0, 0, getWidth(), getHeight(),
-                    0, value * frameHeight_, frameWidth_, frameHeight_);
-        
-        if (isMouseOverOrDragging())
-        {
-        }
+        g.drawImage (filmStrip_,
+                     0,
+                     0,
+                     width_,
+                     height_,
+                     0,
+                     imagePositionRatio * height_,
+                     width_,
+                     height_);
     }
     
-    int getFrameWidth() const  { return frameWidth_; }
-    int getFrameHeight() const { return frameHeight_; }
-    
 private:
+    const Image filmStrip_;
+    const int height_;
+    const int width_;
     const int numFrames_;
-    Image filmStrip_;
-    int frameWidth_;
-    int frameHeight_;
 };
 
 
